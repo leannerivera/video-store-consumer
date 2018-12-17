b22ae05300d69affe16054fcd791884dceffe61f
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-12-17 13:11:20 -0800

    React Router/NPM install, import for Routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {Browser as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
