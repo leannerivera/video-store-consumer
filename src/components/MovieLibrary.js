c71c869ece4e67a68f912d67a9608c006e4fc0d3
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-12-20 23:24:19 -0800

    updated

---
+++ src/components/MovieLibrary.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+
+
+
+class MovieLibrary extends Component {
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    const movies = this.props.movies.map((movie)=>{
+
+      return <MovieCard
+        key={movie.id}
+        id={movie.id}
+        title={movie.title}
+        image_url={movie.image_url}
+        selectMovieCallback={this.props.selectMovieCallback}
+      />
+
+    });
+
+    return (
+
+      <div>
+        <section className="movieLib">
+             {movies}
+        </section>
+      </div>
+    );
+
+  }
+}
+
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.array,
+  selectMovieCallback: PropTypes.func,
+};
+
+export default MovieLibrary;
